7110a13898e26e68e9d3ff894a312382e4b74266
ZihaoBAO <zihao.bao@outlook.com>
2026-07-16 09:10:39 +0800

    add new user miw

--- src/auth/users.js
+++ src/auth/users.js
@@ -8,5 +8,10 @@ export const users = [
     username: "customer",
     role: "customer",
     password: "b041c0aeb35bb0fa4aa668ca5a920b590196fdaf9a00eb852c9b7f4d123cc6d6"
+  },
+  {
+    username: "miw",
+    role: "customer",
+    password: "8589c63b0943a62bfda9b35dccc71a30f5677386f6f7c644c3307465ce2cfa55"
   }
 ];
